87fba5b6b749dafbd041cf37659feab9c287373a
yuvrajthakur1 <yuvrajthakurthakur65@gmail.com>
2025-09-14 20:03:03 +0530

    Make Changes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -79,7 +79,7 @@ const loginController = async (req, res) => {
      res.cookie('token', token, {
       httpOnly: true, // Prevents client-side JavaScript from accessing the cookie
       secure: isProduction, // Use secure cookies in production
-      sameSite:none,
+      sameSite:false,
       maxAge: 3600000, // 1 hour in milliseconds
     });
 
